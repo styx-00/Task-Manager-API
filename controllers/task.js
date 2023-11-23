38074900fbeb4d30d39b4bf26ab9e5732653b548
styx <johithx@gmail.com>
2023-11-23 12:19:25 +0530

    Added controllers

--- controllers/task.js
+++ controllers/task.js
@@ -1,12 +1,34 @@
-const getAllTasks = (req,res)=>{
-    console.log("In route")
-    res.send('all items')
+const Task = require('../models/Task')
+const getAllTasks = async (req,res)=>{
+    try{
+        const task = await Task.find({})
+        res.status(200).json({task})
+    }
+    catch(error){
+        res.status(500).json({msg:error})
+    }
 }
-const createTask = (req,res)=>{
-    res.send('create task');
+const createTask = async (req,res)=>{
+    try{
+        const task = await Task.create(req.body)
+        res.status(201).json({task});
+    }
+    catch(error){
+        res.status(500).json({msg:error});
+    }
 }
-const getTask = (req,res)=>{
-    res.json({id:req.params.id});
+const getTask = async (req,res)=>{
+    try{
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskId})
+        if(!task){
+            return res.status(404).json({msg:`No task with id ${taskId}`})
+        }
+    }
+    catch(error){
+        res.json({msg:error});
+    }
+    
 }
 const updateTask = (req,res)=>{
     res.send('update task');
---
+++ models/Task.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'must provide name'],
+        trim:true,
+        maxlength:[20,'name cannot be more than 20 characters']
+    }
+    ,completed:Boolean
+})
+
+module.exports = mongoose.model('Task',TaskSchema);
